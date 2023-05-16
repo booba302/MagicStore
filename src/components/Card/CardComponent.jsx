fe603de65b19e3f9c739cf7e48a70aa26051ac14
Franklin Sanchez <booba302@gmail.com>
2023-05-16 12:41:55 -0400

    entrega final

--- src/components/Card/CardComponent.jsx
+++ src/components/Card/CardComponent.jsx
@@ -4,7 +4,7 @@ import ButtonComponent from "../Buttons/ButtonComponent";
 
 const CardComponent = (props) => {
   const { data } = props;
-  const { id, name, img, price } = data;
+  const { id, name, img, price, amount } = data;
 
   const showShortValue = (value = "", lengthMax = 45) => {
     return value.length > lengthMax
@@ -14,8 +14,13 @@ const CardComponent = (props) => {
 
   return (
     <div className="card">
-      <div className="card-title">
-        <h2>{showShortValue(name, 20)}</h2>
+      <div className="card-header">
+        <div className="card-title">
+          <h2>{showShortValue(name, 15)}</h2>
+        </div>
+        <div className="card-header-button">
+          <ButtonComponent name="X" className="btn btn-outline-danger" />
+        </div>
       </div>
       <div className="card-img">
         <img src={img} alt="" />
@@ -23,14 +28,17 @@ const CardComponent = (props) => {
       <div className="card-price">
         <h3>Precio: {price}$</h3>
       </div>
+      <div className="card-amount">
+        <h3>Cantidad en existencia: {amount}</h3>
+      </div>
       <div className="card-btns">
         <div className="card-info">
           <NavLink to={`/card/` + id}>
-            <ButtonComponent name="+info" className="btn btn-outline-danger" />
+            <ButtonComponent name="+info" className="btn btn-outline-success" />
           </NavLink>
         </div>
         <div className="card-buy">
-          <ButtonComponent name="Comprar" className="btn btn-outline-success" />
+          <ButtonComponent name="Comprar" className="btn btn-outline-primary" />
         </div>
       </div>
     </div>
--- src/views/DetailsView.jsx
+++ src/views/DetailsView.jsx
@@ -9,38 +9,44 @@ const DetailsView = (props) => {
   const { id: documentId } = useParams();
 
   const [data] = useFirestore({ nameCollection, documentId });
+  const {name, img, description, price, amount} = data;
 
   return (
     <Fragment>
-      <div className="card-details">
-        <div className="card-title">
-          <h2>{data.name}</h2>
-        </div>
-        <div className="card-info">
+      <div className="cards-container">
+        <div className="card-details card">
+          <div className="card-title">
+            <h2>{name}</h2>
+          </div>
+          <div className="card-info">
             <div className="card-img">
-              <img src={data.img} alt="" />
+              <img src={img} alt="" />
             </div>
             <div className="card-text">
-              <p>{data.description}</p>
+              <p>{description}</p>
             </div>
-        </div>
-        <div className="card-price">
-          <h3>Precio: {data.price}$</h3>
-        </div>
-        <div className="card-btns">
-          <div className="btn-info">
-            <ButtonComponent
-              className="btn btn-outline-success"
-              name="comprar"
-            />
           </div>
-          <div className="btn-buy">
-            <NavLink to="/">
+          <div className="card-price">
+            <h3>Precio: {price}$</h3>
+          </div>
+          <div className="card-amount">
+            <h3>Cantidad en existencia: {amount}</h3>
+          </div>
+          <div className="card-btns">
+            <div className="btn-info">
               <ButtonComponent
-                className="btn btn-outline-danger"
-                name="Volver"
+                className="btn btn-outline-primary"
+                name="Comprar"
               />
-            </NavLink>
+            </div>
+            <div className="btn-buy">
+              <NavLink to="/">
+                <ButtonComponent
+                  className="btn btn-outline-danger"
+                  name="Volver"
+                />
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
